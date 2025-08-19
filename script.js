141e53930f40774a58c5ad7b26c56727b23f07eb
Your Name <you@example.com>
2025-08-19 20:39:15 +0330

    opeators

---
+++ script.js
@@ -0,0 +1,30 @@
+let fname="omid", lname="lak";
+
+let fullname=fname.concat("-", " ",lname ," //");
+console.log(fullname);
+
+
+let number3=12, resault;
+resault=Math.pow(number3,2);
+console.log(resault);
+
+
+
+console.log(typeof resault);
+
+let var4=123;
+// OMID: Increment
+// resault= ++var4  ;
+// resault= var4+1;
+// OMID: Decrement
+resault=--var4;
+ console.log(resault);
+
+
+//  resault= resault +1;
+ resault -=1;
+ console.log(resault);
+
+
+ console.log(resault==="121");
+
